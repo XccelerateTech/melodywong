1d545c0fb239dce1953ce20efd71f395e7fa141e
ynmwong <melodysweet@gmail.com>
2018-10-08 14:59:36 +0800

    I not completed yet

--- 06-objects/I.js
+++ 06-objects/I.js
@@ -1,5 +1,5 @@
 //Write a function that contains an array for every set (spades, diamond etc).
-var poker_deck = [
+var deck = [
     ['Spade', 'A'], ['Heart', 'A'], ['Diamond', 'A'], ['Club', 'A'],
     ['Spade', '2'], ['Heart', '2'], ['Diamond', '2'], ['Club', '2'],
     ['Spade', '3'], ['Heart', '3'], ['Diamond', '3'], ['Club', '3'],
@@ -17,24 +17,50 @@ var poker_deck = [
 
 //The function receives two parameter, the deck and the deck value (spades, 8).
 
-function removeCard(card) {
+function luck(){
+    //draw random number of cards
+    var numberOfCard=Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+    console.log("You have " + numberOfCard + " cards:");
+
+    //make an arr
+    var value=[];
+    var whichCard;
+    var newCard;
+    var joinCard;
+    for(var x=0;x<numberOfCard;x++){
+        whichCard=Math.floor(Math.random() * ((52-x) - 1 + 1)) + 1;
+        console.log(whichCard);
+        newCard=deck[whichCard];
+        console.log(newCard);
+        joinCard=newCard;//.join(",");
+        console.log("joinCard"+joinCard);
         
-    return card === ['Heart', 'K'];
+        
+
+            //find cards in deck
+        var a;
+        for(i=0;i<deck.length;i++){
+            var joinDeck=deck[i];//.join(",");
+            if (joinCard===joinDeck){
+                a=i;
+            }
+            }
+            console.log("a"+a);
+            var b=a-1;
+        
+    //Remove the corresponding card from the array.
+            var newDeck=deck.splice(b,a);
+            console.log(newDeck);
+
+    }
+
+
+
+    //Write a function so it takes your initial cards (of the case 1, 2, 3.. until 5 cards) and 
+
+    //calculates the probability of having PAIR, TWO PAIRS and THREE OF A KIND in the the NEXT Card
+
 }
-console.log(poker_deck.findIndex(removeCard));
 
-function luck(deck,value){
-    
-    /*deck=deck.splice((where-1),where);
-    console.log(deck)*/
-}
 
-//Remove the corresponding card from the array.
-
-//You play a limited version of poker, where you only aim for pair, two pair or three of a kind
-
-//Write a function so it takes your initial cards (of the case 1, 2, 3.. until 5 cards) and 
-
-//calculates the probability of having PAIR, TWO PAIRS and THREE OF A KIND in the the NEXT Card
-
-luck(poker_deck,['Spade','8'])
+luck()
