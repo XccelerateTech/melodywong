df7bb4ae82fbf4622e3a34811c0c731c51616580
ynmwong <melodysweet@gmail.com>
2018-10-15 00:34:31 +0800

    updates on G

--- 11-jquery-ajax/G/script.js
+++ 11-jquery-ajax/G/script.js
@@ -1,28 +1,79 @@
+var countryLatlng=[];
 
+function informMe(endpoint){
+ 
+  return $.get({
+      url:`https://restcountries.eu/rest/v2/name/${endpoint}?fullText=true`,
+  })
+
+}
+ 
+ 
  $('.form1').submit(function(event){
     event.preventDefault(); // VERY IMPORTANT. To stop the form from submitting
+    var country1=event.target.country1.value;
+    var country2=event.target.country2.value;
 
+    $.when(informMe(country1), informMe(country2)).then(function (countryData1, countryData2) { //when the two iterations of the function getCountryData are finished. Assign value of returned to two callbacks, countryData1 and countryData2
 
-    var countryLatlng=[];
-    //find country latlng
-    function informMe(endpoint){  
-   
-      return $(function(){ 
+      console.log(countryData1[0][0].latlng)
+      console.log(countryData2[0][0].latlng)
+
+      var country1=countryData1[0][0].latlng;
+      var country2=countryData2[0][0].latlng;
+
+    //get sunrise sunset for calculation
+    function calculate(lat,long){  
+      //get api
+      $(function(){ 
            $.get({
-               url:`https://restcountries.eu/rest/v2/name/${endpoint}?fullText=true`,
+               url:`https://api.sunrise-sunset.org/json?lat=${lat}0&lng=${long}&formatted=0`,
+       
            }).done(function(data){
-            console.log(data[0].latlng)
+              //get time of sunset sunrise
+               Rise= new Date (data.results.sunrise);
+               //sSet= new Date (data.results.sunset);
+             
+
+               
+               /*function msToTime(duration) {
+                  var 
+                    seconds = parseInt((duration / 1000) % 60),
+                    minutes = parseInt((duration / (1000 * 60)) % 60),
+                    hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+                
+                  hours = (hours < 10) ? "0" + hours : hours;
+                  minutes = (minutes < 10) ? "0" + minutes : minutes;
+                  seconds = (seconds < 10) ? "0" + seconds : seconds;
+                
+                  return hours + ":" + minutes + ":" + seconds
+                }*/
+          
+          
+              return (Rise)
+          
            }).fail(function(data){
-               console.log("Error");
+              $('.result').append("Error");
            })
            
        });
-   
-   }
-   $.when(informMe(event.target.country1.value), informMe(event.target.country2.value)).done(function(val1,val2){
-     console.log(val1);
-     console.log(val2);
-   })
+
+      }
+      //end of function 
+      $.when(calculate(country1[0],country1[1]), calculate(country2[0],country2[1])).then(function (countryData1, countryData2) { 
+        console.log(countryData1);
+
+      })
+
+
+
+    })
+  })
+  
+
+  
+     
+
   
   
 
@@ -35,64 +86,7 @@
     
 
 
-    //get sunrise sunset for calculation
-    function calculate(lat,long,day){  
-        //get api
-        $(function(){ 
-             $.get({
-                 url:`https://api.sunrise-sunset.org/json?lat=${lat}0&lng=${long}&date=${day}&formatted=0`,
-         
-             }).done(function(data){
-                //get time of sunset sunrise
-                 Rise= new Date (data.results.sunrise);
-                 sSet= new Date (data.results.sunset);
-                 var now = Date.now();
-                 
-                 function msToTime(duration) {
-                    var milliseconds = parseInt((duration % 1000) / 100),
-                      seconds = parseInt((duration / 1000) % 60),
-                      minutes = parseInt((duration / (1000 * 60)) % 60),
-                      hours = parseInt((duration / (1000 * 60 * 60)) % 24);
-                  
-                    hours = (hours < 10) ? "0" + hours : hours;
-                    minutes = (minutes < 10) ? "0" + minutes : minutes;
-                    seconds = (seconds < 10) ? "0" + seconds : seconds;
-                  
-                    return hours + ":" + minutes + ":" + seconds
-                  }
-                  
-                  if(day==='yesterday'){
-                    $('.result').append(`Time between NOW and yesterday sunrise is `);
-                  } else {
-                    $('.result').append(`Time between NOW and tomorrow sunrise is `);
-                  }
-
-                  if(now>Rise){
-                    $('.result').append(msToTime(now-Rise))
-                  } else {
-                    $('.result').append(msToTime(Rise-now))
-                  }
-
-                  
-                $('.result').append(` and sunset is `);
-                  
-                  
-                  if(now>sSet){
-                    $('.result').append(msToTime(now-sSet)+`</p>`)
-                  } else {
-                    $('.result').append(msToTime(sSet-now)+`</p>`)
-                  }
-
-                
-            
-             }).fail(function(data){
-                $('.result').append("Error");
-             })
-             
-         });
-
-        }
-
+    
     
 
     
@@ -102,6 +96,3 @@
     calculate(lat,long,'tomorrow','&formatted=0'); //today sunrise time cal
 
 */
- })
-
- 
