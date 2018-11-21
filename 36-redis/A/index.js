4631338c6b06bcc45c78cb23ff2a8502a8fcbac5
ynmwong <melodysweet@gmail.com>
2018-11-21 16:24:36 +0800

    finished A

--- 36-redis/A/index.js
+++ 36-redis/A/index.js
@@ -2,7 +2,16 @@
 //txindex 
 //read latest block every 10 minutes
 
-var redis = require('redis');
+let redis = require('redis');
+let async=require('async');
+let axios=require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+let app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 var client = redis.createClient({
     host: 'localhost',
     port: 6379,
@@ -13,22 +22,59 @@ client.on('error', function(err){
     console.log(err);
 });
 
-client.auth('password', function(err, data) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("entered password")
-    });
+let index=[];
+let num=0;
 
-client.setex('location', 60, 'Hong Kong', function(err, data) {
-    if(err) {
-        return console.log(err);
-    }
+axios.get('https://blockchain.info/latestblock')
+  .then(function (response) {
+    index=response.data.txIndexes;
+    
+    console.log("Got latest block");
+
+  })
+  .then(function () {
+
+
+    async.eachSeries(index, function (item, next) { 
+      setTimeout(function() {
+           axios.get(`https://blockchain.info/rawtx/${item}`)
+            .then(function(data){
+                let push=data.data.hash;
+                console.log("hash: ",data.data.hash)
+                client.lpush('txIndex', push,function(err,data) {
+                      
+                        
+                        if(err) {
+                          console.log("lpush-error")
+                          return console.log(err);
+                      }
+                    })
+              })
+           next(); // don't forget to execute the callback!
+      }, 100);
+    }, function () {
+          console.log('Done going through all transactions!');
+    });
+    })
+
+
+  .then(function(){
+    client.llen('txIndex',function(err,data){
+      console.log("length",data)
+    })
+    client.expire('txIndex',1,function(err,data){
+
+      if(err) {
+          console.log("expire-error")
+          return console.log(err);
+      }
+    })
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+app.listen(3000,()=>{
+    console.log("Application started at port:3000");
 });
-
-client.get('location', function(err, data){
-        if(err) {
-            return console.log(err);
-        }
-        console.log('The value is ', data);
-    });
